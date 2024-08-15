cdfc5cc388446b542f43f02e29bb6ded56deeba5
Neel Champaneria <neel@promactinfo.com>
2024-08-15 13:11:03 +0530

    Change db ts

--- libs/db.ts
+++ libs/db.ts
@@ -4,23 +4,21 @@
 
 // export default db;
 
-import { Pool, neonConfig } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 // import dotenv from "dotenv";
-import ws from "ws";
+// import ws from "ws";
 
 // dotenv.config();
-neonConfig.webSocketConstructor = ws;
-const connectionString =
-  process.env.NODE_ENV === "development"
-    ? `${process.env.POSTGRES_PRISMA_URL}`
-    : `${process.env.POSTGRES_URL_NON_POOLING}`;
+// neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.POSTGRES_PRISMA_URL}`;
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
-let db;
+let db: PrismaClient =
+  process.env.NODE_ENV === "development"
+    ? new PrismaClient()
+    : new PrismaClient({ adapter });
 
-export default process.env.NODE_ENV === "development"
-  ? new PrismaClient()
-  : new PrismaClient({ adapter });
+export default db;
