c47556e1fff1ac07b1a65f84f4bdf9595d3cd941
Neel Champaneria <neel@promactinfo.com>
2024-08-15 13:15:31 +0530

    Configute runtime

--- app/api/thoughts/route.ts
+++ app/api/thoughts/route.ts
@@ -5,6 +5,8 @@ import { NextRequest, NextResponse } from "next/server";
 // export const runtime =
 //   process.env.NODE_ENV === "development" ? "nodejs" : "edge";
 
+export const runtime = "edge";
+
 export async function POST(request: NextRequest) {
   const { text } = await request.json();
   const thought = await db.thought.create({
