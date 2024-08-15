d3b92f773d40ac35f796b8c6df18ee199bb48a31
Neel Champaneria <neel@promactinfo.com>
2024-08-15 11:41:12 +0530

    Prisma adapter

--- libs/db.ts
+++ libs/db.ts
@@ -1,5 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const db = new PrismaClient();
+// const db = new PrismaClient();
+
+// export default db;
+
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import ws from "ws";
+
+dotenv.config();
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.POSTGRES_PRISMA_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+const db = new PrismaClient({ adapter });
 
 export default db;
